0f7140c5100a3f254bcd9f1b8922b8c60f10f81e
SnehaGoel2004 <goelsneha.0910@gmail.com>
2025-03-01 17:19:15 +0000

    Updated frontend to use deployed backend API

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import News from "./components/NewsList";
 import './App.css'; // Import the CSS file
+import NewsList from "./components/NewsList";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<News />} />
+                <Route path="/news" element={<NewsList />} />
                 
                 {/* You can add more routes for different categories here */}
                 {/* Example: <Route path="/technology" element={<Technology />} /> */}
